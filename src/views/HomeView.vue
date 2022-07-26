05b1bc02da1ecb8a7d8dbed11e4fb02c10c125df
KatherineEvans <Evans.Katherine.E@gmail.com>
2022-07-25 19:27:46 -0500

    create hardcoded recipes and display using a v-for loop

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,28 @@
+<script>
+export default {
+  data: function () {
+    return {
+      message: "All Recipes",
+      recipes: [
+        { id: 1, title: "Raw Eggs", chef: "Jay Wengrow" },
+        { id: 1, title: "Empty Sandwich", chef: "Jay Wengrow" },
+        { id: 1, title: "Ice", chef: "Peter Jang" },
+      ],
+    };
+  },
+  created: function () {},
+  methods: {},
+};
+</script>
+
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <h1>{{ message }}</h1>
+    <div v-for="recipe in recipes" v-bind:key="recipe.id">
+      <h2>Title: {{ recipe.title }}</h2>
+      <p>Chef: {{ recipe.chef }}</p>
+    </div>
   </div>
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
-export default {
-  name: "HomeView",
-  components: {
-    HelloWorld,
-  },
-};
-</script>
+<style></style>
