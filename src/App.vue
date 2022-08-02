f92bd925f795b8deb61e67617dfed08341e605a1
KatherineEvans <Evans.Katherine.E@gmail.com>
2022-08-02 18:53:31 -0500

    add bootstrap - navbar, cards, font, and background image

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,48 @@
 <template>
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">Navbar</a>
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Link</a>
+          </li>
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+              Dropdown
+            </a>
+            <ul class="dropdown-menu">
+              <li><a class="dropdown-item" href="#">Action</a></li>
+              <li><a class="dropdown-item" href="#">Another action</a></li>
+              <li><hr class="dropdown-divider" /></li>
+              <li><a class="dropdown-item" href="#">Something else here</a></li>
+            </ul>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link disabled">Disabled</a>
+          </li>
+        </ul>
+        <form class="d-flex" role="search">
+          <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+          <button class="btn btn-outline-success" type="submit">Search</button>
+        </form>
+      </div>
+    </div>
+  </nav>
   <nav>
     <router-link to="/">Home</router-link>
     |
@@ -14,28 +58,14 @@
     |
     <router-link to="/logout">Logout</router-link>
   </nav>
-  <router-view />
+  <div class="container">
+    <router-view />
+  </div>
 </template>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
+body {
+  font-family: Futura, Trebuchet MS, Arial, sans-serif;
+  background-image: url("https://www.toptal.com/designers/subtlepatterns/uploads/concrete_wall_2.png");
 }
 </style>
--- src/views/RecipesIndex.vue
+++ src/views/RecipesIndex.vue
@@ -23,13 +23,30 @@ export default {
 <template>
   <div>
     <h1>All Recipes</h1>
-    <div v-for="recipe in recipes" v-bind:key="recipe.id">
+    <div class="row">
+      <div class="col-sm-4" v-for="recipe in recipes" v-bind:key="recipe.id">
+        <div class="card mb-4">
+          <img class="card-img-top" v-bind:src="recipe.image_url" v-bind:alt="recipe.title" />
+          <div class="card-body">
+            <h5 class="card-title">{{ recipe.title }}</h5>
+            <p class="card-text">Chef: {{ recipe.chef }}</p>
+            <a v-bind:href="`/recipes/${recipe.id}`" class="btn btn-primary">More Info</a>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- <div v-for="recipe in recipes" v-bind:key="recipe.id">
       <h2>Title: {{ recipe.title }}</h2>
       <img v-bind:src="recipe.image_url" v-bind:alt="recipe.title" />
       <p>Chef: {{ recipe.chef }}</p>
       <a v-bind:href="`/recipes/${recipe.id}`">More info</a>
-    </div>
+    </div> -->
   </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.card img {
+  object-fit: cover;
+  height: 250px;
+}
+</style>
